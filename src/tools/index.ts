241c83673169089aa254c58e805e2e86b71a6076
RohanMuppa <rohanmuppawork@gmail.com>
2026-02-08 03:26:25 -0500

    register get_assignments tool in MCP server and barrel export

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -3,6 +3,7 @@ export { registerGetMyCourses } from "./get-my-courses.js";
 export { registerGetUpcomingDueDates } from "./get-upcoming-due-dates.js";
 export { registerGetMyGrades } from "./get-my-grades.js";
 export { registerGetAnnouncements } from "./get-announcements.js";
+export { registerGetAssignments } from "./get-assignments.js";
 
 // Re-export shared helpers and schemas for convenience
 export { toolResponse, errorResponse, sanitizeError } from "./tool-helpers.js";
